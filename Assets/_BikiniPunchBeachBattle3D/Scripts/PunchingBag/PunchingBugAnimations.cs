07a61464cf6d0a836f5de10f057c68ae92cd422e
redHurt <62645756+redHurt96@users.noreply.github.com>
2022-07-28 13:40:22 +0300

    Fix animations

--- Assets/_BikiniPunchBeachBattle3D/Scripts/PunchingBag/PunchingBugAnimations.cs
+++ Assets/_BikiniPunchBeachBattle3D/Scripts/PunchingBag/PunchingBugAnimations.cs
@@ -2,15 +2,15 @@ using _BikiniPunchBeachBattle3D.Characters;
 using _BikiniPunchBeachBattle3D.GameServices;
 using RH.Utilities.ServiceLocator;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace _BikiniPunchBeachBattle3D.PunchingBag
 {
     public class PunchingBugAnimations : MonoBehaviour
     {
-        private static readonly string[] _triggersNames = {"Hit_0", "Hit_1", "Hit_2", "Hit_3"};
+        private static readonly string[] _triggersNames = {"Hit_0", "Hit_1"};
         
-        [SerializeField] private Animator _animator;
-        
+        private Animator _animator;
         private EventsMediator _events;
 
         private void Start()
@@ -19,7 +19,13 @@ namespace _BikiniPunchBeachBattle3D.PunchingBag
             _events.PunchPerformed.AddListener(Animate);
         }
 
-        private void Animate(CharacterType characterType, string side) => 
+        private void OnDestroy() => 
+            _events.PunchPerformed.RemoveListener(Animate);
+
+        private void Animate(CharacterType characterType, string side)
+        {
+            _animator ??= GetComponentInChildren<Animator>();
             _animator.SetTrigger(_triggersNames[Random.Range(0, _triggersNames.Length)]);
+        }
     }
 }
